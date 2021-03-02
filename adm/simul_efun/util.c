d532cbbb9a2f8fcb77fbfdf4385f8ffef9cd9cf6
噢哎哟喂 <i@oiuv.cn>
2021-03-02 16:01:05 +0800

    upate config.cfg

--- adm/simul_efun/util.c
+++ adm/simul_efun/util.c
@@ -2,6 +2,39 @@
 #include <ansi.h>
 #include <localtime.h>
 
+// 模拟 fluffos 新增 efun
+#ifndef FLUFFOS
+
+mixed abs(mixed n)
+{
+    if (!floatp(n) && !intp(n))
+        return 0;
+
+    return (n < 0) ? -n : n;
+}
+
+mixed element_of(mixed *arr)
+{
+    return arr[random(sizeof(arr))];
+}
+
+#endif
+
+#ifndef __PACKAGE_TRIM__
+// 去掉str两端的空格
+string trim(string str)
+{
+    int len;
+
+    while (str[0] == ' ')
+        str = str[1.. < 1];
+    while ((len = strlen(str) - 1) >= 0 && str[len] == ' ')
+        str = str[0.. < 2];
+
+    return str;
+}
+#endif
+
 void debug(mixed arg)
 {
         string *color = ({HIB, HIC, HIG, HIM, HIR, HIW, HIY});
